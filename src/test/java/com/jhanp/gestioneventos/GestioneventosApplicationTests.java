e71cbfd563278efe3fc9fae05c61283f1ff07eb3
Your Name <you@example.com>
2026-04-10 13:10:53 -0500

    Initial commit - Spring Boot project

---
+++ src/test/java/com/jhanp/gestioneventos/GestioneventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhanp.gestioneventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
